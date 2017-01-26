f4b7f4bf87c95eaf38560f8ff057859d4825104a
MouaCoding <mouacoding@yahoo.com>
2017-01-26 13:04:06 -0800

    Added Megaphone to bottom bar, added click functionality

---
+++ app/src/main/java/com/example/matt/RightHereRightNow/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.matt.RightHereRightNow;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem bottom_navigation) {
+                        switch (bottom_navigation.getItemId()) {
+                            case R.id.map_pin:
+                                //do something when user clicks on each
+                                break;
+                            case R.id.megaphone:
+
+                                break;
+                            case R.id.menu:
+
+                                break;
+                            case R.id.music_social_group:
+
+                                break;
+                            case R.id.identity_card:
+
+                                break;
+                        }
+                        return true;
+                    }
+                });
+
+    }
+
+
+
+}
