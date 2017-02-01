c6dc58a9990a94b65473e621c5a714a8c9d4d8f7
MouaCoding <mouacoding@yahoo.com>
2017-01-31 21:43:24 -0800

    Can navigate bottom bar on click

--- app/src/main/java/com/example/matt/RightHereRightNow/PostActivity.java
+++ app/src/main/java/com/example/matt/RightHereRightNow/PostActivity.java
@@ -1,19 +1,24 @@
 package com.example.matt.RightHereRightNow;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class PostActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.profile_page_temp_layout);
+        setContentView(R.layout.activity_post);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView)
                 findViewById(R.id.bottom_navigation);
@@ -49,8 +54,14 @@ public class MainActivity extends AppCompatActivity {
                 });
 
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
-
-
 
 }
